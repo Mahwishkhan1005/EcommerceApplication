0f86a24092fe1e63e6cd76e892e2247de7d652fe
Monuchauhan80 <157130306+Monuchauhan80@users.noreply.github.com>
2025-12-23 09:20:59 +0530

    update

--- app/(utils)/axiosInstance.tsx
+++ app/(utils)/axiosInstance.tsx
@@ -1,15 +1,17 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios, {
-    AxiosError,
-    AxiosInstance,
-    InternalAxiosRequestConfig,
+  AxiosError,
+  AxiosInstance,
+  InternalAxiosRequestConfig,
 } from 'axios';
  
 
 
 const getToken = async (): Promise<string | null> => {
   try {
-    return await AsyncStorage.getItem("AccessToken");
+    const token = await AsyncStorage.getItem("AccessToken");
+    console.log('Token from AsyncStorage:', token ? 'Token exists' : 'No token');
+    return token;
   } catch (error) {
     console.error("Error getting token:", error);
     return null;
@@ -28,9 +30,11 @@ const createAxiosInstance = (baseURL: string): AxiosInstance => {
   instance.interceptors.request.use(
     async (config: InternalAxiosRequestConfig): Promise<InternalAxiosRequestConfig> => {
       const token: string | null = await getToken();
+      console.log('Retrieved token:', token ? 'Token exists' : 'No token');
       
       if (token) {
         config.headers.Authorization = `Bearer ${token}`;
+        console.log('Added Authorization header');
       }
       return config;
     },
@@ -40,4 +44,7 @@ const createAxiosInstance = (baseURL: string): AxiosInstance => {
   return instance;
 };
 
-export const rootApi: AxiosInstance = createAxiosInstance("http://192.168.0.246:8080");
+export const rootApi: AxiosInstance = createAxiosInstance("http://192.168.0.200:8083");
+export const adressApi: AxiosInstance = createAxiosInstance("http://192.168.0.200:8083");
+export const cartApi:AxiosInstance=createAxiosInstance("http://192.168.0.200:8082");
+export const payApi:AxiosInstance=createAxiosInstance("http://192.168.0.186:8082");
