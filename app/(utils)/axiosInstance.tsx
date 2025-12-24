c08bc3c073dd978d839fe79d1f5598a2bf272370
mahwish <mahwishkhan1005@gmail.com>
2025-12-24 10:52:44 +0530

    Updated:

--- app/(utils)/axiosInstance.tsx
+++ app/(utils)/axiosInstance.tsx
@@ -44,7 +44,7 @@ const createAxiosInstance = (baseURL: string): AxiosInstance => {
   return instance;
 };
 
-export const rootApi: AxiosInstance = createAxiosInstance("http://192.168.0.200:8083");
-export const adressApi: AxiosInstance = createAxiosInstance("http://192.168.0.200:8083");
-export const cartApi:AxiosInstance=createAxiosInstance("http://192.168.0.200:8082");
-export const payApi:AxiosInstance=createAxiosInstance("http://192.168.0.186:8082");
+export const rootApi: AxiosInstance = createAxiosInstance("http://192.168.0.225:8083");
+export const adressApi: AxiosInstance = createAxiosInstance("http://192.168.0.225:8083");
+export const cartApi:AxiosInstance=createAxiosInstance("http://192.168.0.225:8082");
+export const payApi:AxiosInstance=createAxiosInstance("http://192.168.0.215:8082");
